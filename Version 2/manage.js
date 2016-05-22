7e7570302ea215d2c740be90b29543792a1de265
Siderite <siderite@madnet.ro>
2016-05-22 18:45:10 +0300

    bug fixes

--- Version 2/manage.js
+++ Version 2/manage.js
@@ -1,49 +1,50 @@
 (function ($) {
 
 	var global = this;
-	var context = global.testContext&&global.testContext.document || global.document;
-	var chrome = global.testContext && global.testContext.chrome? global.testContext.chrome : global.chrome;
-	var confirm=global.testContext &&global.testContext.confirm ? global.testContext.confirm : global.confirm;
+	var context = global.testContext && global.testContext.document || global.document;
+	var chrome = global.testContext && global.testContext.chrome ? global.testContext.chrome : global.chrome;
+	var confirm = global.testContext && global.testContext.confirm ? global.testContext.confirm : global.confirm;
 
 	function createTree(itm, level, checkData) {
 		var elem = $('<div></div>');
 		if (itm.children) {
-			if (level > 1)
-				elem.addClass('subtree');
 			elem.append($('<h' + level + '></h' + level + '>')
-				.text(itm.title))
-				.append($('<div id="divCounts"><span></span></div>'));
+				.text(itm.title));
+			if (level > 1) {
+				elem.addClass('subtree');
+			} else {
+				elem.append($('<div id="divCounts"><span></span></div>'));
+			}
 			var ul = $('<ul></ul>').appendTo(elem);
 			itm.children.forEach(function (child) {
 				$('<li></li>')
 				.append(createTree(child, level + 1, checkData))
 				.appendTo(ul);
 			});
-		} else
-			if (itm.url) {
+		} else {
+			if (itm.url && level == 2) {
 				$('<a></a>')
 				.text(itm.title || itm.url)
 				.prepend($('<img/>').attr('src', ApiWrapper.getIconForUrl(itm.url)))
 				.attr('href', itm.url || '#')
 				.attr('target', '_blank')
 				.appendTo(elem);
-				if (level == 2) {
-					var chk = $('<input />')
-						.attr('type', 'checkbox')
-						.val(itm.id)
-						.attr('title', 'Mark for delete')
-						.appendTo(elem);
-					chk.data('id',itm.id);
-					if (checkData&&checkData[itm.id]) {
-						chk.prop('checked',true);
-					}
-					elem.click(function (ev) {
-						if (!chk.is(ev.target)) {
-							chk.click();
-						}
-					});
+				var chk = $('<input />')
+					.attr('type', 'checkbox')
+					.val(itm.id)
+					.attr('title', 'Mark for delete')
+					.appendTo(elem);
+				chk.data('id', itm.id);
+				if (checkData && checkData[itm.id]) {
+					chk.prop('checked', true);
 				}
+				elem.click(function (ev) {
+					if (!chk.is(ev.target)) {
+						chk.click();
+					}
+				});
 			}
+		}
 		return elem;
 	}
 
@@ -59,7 +60,7 @@
 	$(function () {
 
 		var api = new ApiWrapper(chrome);
-		var container = $('html,body', context);
+		var container = $('body', context);
 		var tree = $('#tree', context);
 		var buttons = $('#buttons', context);
 		var btnToggleAll = $('#btnToggleAll', context);
@@ -67,28 +68,44 @@
 		var btnRemove = $('#btnRemove', context);
 		var btnUndo = $('#btnUndo', context);
 
-		$(context).on('keyup',function(e) {
-			var ul = tree.find('>div>ul');
-			var index = ul.find('>li:has(div.selected)').index();
-			if (index<0) return;
-			switch(e.which) {
-				case 38: //Up
-					if (index-1>=0) {
-						ul.find('>li>div').eq(index).removeClass('selected');
-						ul.find('>li>div').eq(index-1).addClass('selected');
-					}
-					break;
-				case 40: //Down
-					var total=ul.find('>li').length;
-					if (index+1<total) {
-						ul.find('>li>div').eq(index).removeClass('selected');
-						ul.find('>li>div').eq(index+1).addClass('selected');
-					}
-					break;
-				case 32: //Space
-					ul.find('>li').eq(index).find('input[type=checkbox]').click();
-					break;
+		$(context).on('keydown', function (e) {
+			switch (e.which) {
+			case 38: //Up
+			case 40: //Down
+			case 32: //Space
+				e.preventDefault();
+				break;
 			}
+			
+		});
+
+		$(context).on('keyup', function (e) {
+			var ul = tree.find('>div>ul');
+			var index = ul.find('>li:has(div.current)').index();
+			if (index < 0) index=0;
+			switch (e.which) {
+			case 38: //Up
+				if (index - 1 >= 0) {
+					ul.find('>li>div').eq(index).removeClass('current');
+					index--;
+					ul.find('>li>div').eq(index).addClass('current');
+					bringCurrentIntoView();
+				}
+				break;
+			case 40: //Down
+				var total = ul.find('>li').length;
+				if (index + 1 < total) {
+					ul.find('>li>div').eq(index).removeClass('current');
+					index++;
+					ul.find('>li>div').eq(index).addClass('current');
+					bringCurrentIntoView();
+				}
+				break;
+			case 32: //Space
+				ul.find('>li').eq(index).find('input[type=checkbox]').click();
+				break;
+			}
+			
 		});
 
 		api.onMessage(refresh);
@@ -102,12 +119,12 @@
 				}
 			});
 
-			var checkData={};
-			tree.find('input[type=checkbox]').each(function() {
-				var id=$(this).data('id');
+			var checkData = {};
+			tree.find('input[type=checkbox]').each(function () {
+				var id = $(this).data('id');
 				if (id) {
-					var checked=$(this).prop('checked');
-					checkData[id]=checked;
+					var checked = $(this).prop('checked');
+					checkData[id] = checked;
 				}
 			});
 			currentData = data;
@@ -122,17 +139,25 @@
 			}
 			btnToggleAll.show();
 			btnToggleBefore.show();
-			var bottom = buttons.offset().top;
-			tree.append(createTree(data.folder, 1,checkData));
-			tree.find('input').click(refreshbtnRemove);
-			var minTop = null;
-			var maxTop = null;
+			tree.append(createTree(data.folder, 1, checkData));
+			tree.find('input[type=checkbox]').click(refreshbtnRemove);
 			tree.find('a').each(function () {
-				if (ApiWrapper.sameUrls($(this).attr('href'), data.current.url) < 2) return;
+				if (ApiWrapper.sameUrls($(this).attr('href'), data.current.url) < 2)
+					return;
 
 				var par = $(this).parent();
 				par.addClass('selected');
-				var ofs = par.offset();
+			});
+			bringSelectedIntoView();
+			refreshCounts();
+		}
+
+		function bringSelectedIntoView() {
+			var bottom = $(window).height()-buttons.height();
+			var minTop = null;
+			var maxTop = null;
+			tree.find('div.selected').each(function () {
+				var ofs = $(this).offset();
 				if (minTop === null)
 					minTop = ofs.top;
 				if (maxTop === null || ofs.top + 25 > maxTop)
@@ -146,7 +171,19 @@
 					});
 				}
 			}
-			refreshCounts();
+		}
+
+		function bringCurrentIntoView() {
+			var bottom = $(window).height()-buttons.height();
+			var current = tree.find('div.current');
+			var minTop=current.offset().top;
+			var maxTop=minTop+current.height();
+			if (minTop && maxTop) {
+				var y = (minTop + maxTop) / 2;
+				container.clearQueue().animate({
+					scrollTop : y - bottom / 2
+				});
+			}
 		}
 
 		function refreshbtnRemove() {
@@ -161,9 +198,8 @@
 			var inputs = ul.find('input');
 			var checkedInputs = ul.find('input:checked');
 			tree.find('#divCounts span').text(inputs.length
-				? checkedInputs.length+'/'+inputs.length
-				: ''
-			);
+				 ? checkedInputs.length + '/' + inputs.length
+				 : '');
 		}
 
 		btnToggleAll.click(function () {
@@ -188,7 +224,7 @@
 				return;
 			var ids = []
 			inputs.each(function () {
-				var id =  $(this).val();
+				var id = $(this).val();
 				if (id) {
 					ids.push({
 						id : id,
